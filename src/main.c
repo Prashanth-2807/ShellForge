1531eb7b81172088e2900ca7df726495e6725d72
Prashanth-2807 <ksrisaiprashanth@gmail.com>
2026-09-12 08:30:28 +0000

    Week 6: Added signal handling

--- src/main.c
+++ src/main.c
@@ -7,11 +7,13 @@
 #include "../include/parser.h"
 #include "../include/process.h"
 #include "../include/builtin.h"
+#include "../include/signals.h"
 
 int main()
 {
 char *line;
 char **tokens;
+initialize_signals();
 while(1)
 {
     printf("myshell> ");
